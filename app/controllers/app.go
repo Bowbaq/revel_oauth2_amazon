7e677531f2e32099d43899dc715e3a8165d14992
Maxime Bury <maxime.bury@gmail.com>
2013-11-10 03:43:31 -0500

    Add oauth demo

--- app/controllers/app.go
+++ app/controllers/app.go
@@ -1,11 +1,78 @@
 package controllers
 
-import "github.com/robfig/revel"
+import (
+	"code.google.com/p/goauth2/oauth"
+	"encoding/json"
+	"fmt"
+	"github.com/robfig/revel"
+	"revel_oauth2_amazon/app/models"
+	"net/http"
+	"net/url"
+	"strconv"
+)
 
 type App struct {
 	*revel.Controller
 }
 
-func (c App) Index() revel.Result {
-	return c.Render()
+var AMAZON = &oauth.Config{
+	ClientId:     "amzn1.application-oa2-client.b0ca63fd94cd4e0ab836d61529fa49ac",
+	ClientSecret: "6755fce240af835e2461c2352480ab713fbc6ee01ee17dedeb88ffdbc1ad281b",
+	Scope:        "profile",
+	AuthURL:      "https://www.amazon.com/ap/oa",
+	TokenURL:     "https://api.amazon.com/auth/o2/token",
+	RedirectURL:  "https://revel-oauth2-amazon.herokuapp.com/login",
+}
+
+func (app App) Index() revel.Result {
+	user := app.currentUser()
+	profile := map[string]interface{}{}
+	if user != nil && user.Token.AccessToken != "" {
+		resp, _ := http.Get("https://api.amazon.com/user/profile?access_token=" +
+			url.QueryEscape(user.Token.AccessToken))
+		defer resp.Body.Close()
+		if err := json.NewDecoder(resp.Body).Decode(&profile); err != nil {
+			revel.ERROR.Println(err)
+		}
+		revel.INFO.Println(profile)
+	}
+
+	authUrl := AMAZON.AuthCodeURL("access_request")
+	return app.Render(profile, authUrl)
+}
+
+func (app App) Login(code string) revel.Result {
+	transport := &oauth.Transport{Config: AMAZON}
+	token, err := transport.Exchange(code)
+	if err != nil {
+		revel.ERROR.Println(err)
+		return app.Redirect(App.Index)
+	}
+
+	user := app.currentUser()
+  models.SetToken(user.Uid, token)
+
+	return app.Redirect(App.Index)
+}
+
+func setUser(app *revel.Controller) revel.Result {
+	var user *models.User
+	if _, ok := app.Session["uid"]; ok {
+		uid, _ := strconv.ParseInt(app.Session["uid"], 10, 0)
+		user = models.GetUser(uint64(uid))
+	}
+	if user == nil {
+		user = models.NewUser()
+		app.Session["uid"] = fmt.Sprintf("%d", user.Uid)
+	}
+	app.RenderArgs["user"] = user
+	return nil
+}
+
+func init() {
+	revel.InterceptFunc(setUser, revel.BEFORE, &App{})
+}
+
+func (c App) currentUser() *models.User {
+  return c.RenderArgs["user"].(*models.User)
 }
---
+++ app/models/user.go
@@ -0,0 +1,51 @@
+package models
+
+import (
+	"code.google.com/p/goauth2/oauth"
+  "sync"
+  "fmt"
+)
+
+type User struct {
+	Uid uint64
+	Token oauth.Token
+}
+
+var db = struct {
+  users map[uint64]*User
+  next_id uint64
+  sync.RWMutex
+}{
+  users: make(map[uint64]*User),
+}
+
+func GetUser(id uint64) *User {
+  db.RLock()
+  user := db.users[id]
+  db.RUnlock()
+
+  return user
+}
+
+func NewUser() *User {
+  db.Lock()
+  db.next_id += 1
+  user := &User{ Uid: db.next_id }
+	db.users[user.Uid] = user
+  db.Unlock()
+
+  return user
+}
+
+func SetToken(id uint64, token *oauth.Token) error {
+  user := GetUser(id)
+  if user == nil {
+    return fmt.Errorf("Couldn't find user with id: %d", id)
+  }
+
+  db.Lock()
+  db.users[user.Uid].Token = *token
+  db.Unlock()
+
+  return nil
+}
